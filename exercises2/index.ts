44561218da2b8aa0a9d6d24dd75a5912a34e92f0
Malcolm Macleod <malcolm@macleodhome.com>
2021-05-16 17:32:38 +0100

    9.9 - step 2 - wire up with frontend

--- exercises2/index.ts
+++ exercises2/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
 
-app.get('/ping', (_req, res) => {
+const PORT = 3001;
+
+app.get('/api/ping', (_req, res) => {
     console.log('someone pinged here');
     res.send('pong');
 });
